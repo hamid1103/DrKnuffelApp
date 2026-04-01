1f06b500ade2005edd41e457dd4277605c5cc4de
hamid1103 <35217917+hamid1103@users.noreply.github.com>
2026-04-01 14:33:30 +0200

    Working on Subtitle tool

---
+++ Assets/Scripts/SubtitlerClass.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SubtitlerClass : MonoBehaviour
+    {
+        private GameManager _gameManager;
+        private TMP_Text _text;
+        public string CurrentString;
+        
+        void Start()
+        {
+            _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+            _text = gameObject.GetComponent<TMP_Text>();
+            _text.text = CurrentString;
+        }
+
+        public void SetText(string input)
+        {
+            
+        }
+
+        private string ParseString(string input)
+        {
+
+            List<string> splits = input.Split().ToList();
+            for (int i = 0; i < splits.Count-1; i++)
+            {
+                string text = splits[i];
+                if (text.Contains("{{User}}"))
+                {
+                    if (text.Last() == Convert.ToChar(","))
+                    {
+                        splits[i] = $"{_gameManager.UserName},";
+                    }
+                    /*else if ()
+                    {
+                        
+                    }*/
+                    else
+                    {
+                        splits[i] = $"{_gameManager.UserName}";
+                    }
+                }
+            }
+            return "";
+        }
+    }
+}
